2943c161f3cbe271bf310eaafd001b09d616ed44
Vanessa <Vaness@dorbs>
2018-07-28 21:39:12 -0500

    more webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
 
-const moduleObj = {
+const client = {
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'src', 'public'),
+    compress: true,
+    port: 80,
+    open: true
+  },
+  entry: {
+    'client': './src/client/client.js'
+  },
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist', 'public')
+  },
+  module: {
     rules: [
       {
         test: /\.js$/,
@@ -12,42 +27,31 @@ const moduleObj = {
         }
       }
     ]
-}
-
-const client = {
-    devtool: 'source-map',
-    devServer: {
-      contentBase: path.join(__dirname, 'dist', 'public'),
-      compress: true,
-      port: 80
-    },
-    entry: {
-        'client': './src/client/client.js'
-    },
-    target: 'web',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist/public')
-    },
-    module: moduleObj,
-    plugins: [
-      new HtmlWebPackPlugin({
-        template: 'src/client/index.html'
-      })
-  ]
+  }
 }
 
 const server = {
-    devtool: 'source-map',
-    entry: {
-        'server': './src/server/server.js'
-    },
-    target: 'node',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    externals: [nodeExternals()]
+  devtool: 'source-map',
+  entry: {
+    'server': './src/server/server.js'
+  },
+  target: 'node',
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist')
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 }
 
 module.exports = [client, server]
